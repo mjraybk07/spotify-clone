2f5f7e47cf9d46f3864d0552c4b728916815b25b
mjraybk07 <michaelflis07@gmail.com>
2022-05-22 10:28:36 -0700

    adds hooks for fetching user and playlist data, with swr

---
+++ lib/hooks.ts
@@ -0,0 +1,24 @@
+import useSWR from 'swr';
+import fetcher from './fetcher';
+
+// hook to get user
+export const useMe = () => {
+  const { data, error } = useSWR('/me', fetcher);
+
+  return {
+    user: data,
+    isLoading: !data && !error,
+    isError: error,
+  }
+}
+
+// hook to get playlist for sidebar
+export const usePlaylist = () => {
+  const { data, error } = useSWR('/playlist', fetcher);
+
+  return {
+    playlists: data || [],
+    isLoading: !data && !error,
+    isError: error,
+  }
+}
