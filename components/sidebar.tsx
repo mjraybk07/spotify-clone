adb876a5cf856cd6b83e184c14e9603fe1ca74ed
mjraybk07 <michaelflis07@gmail.com>
2022-05-28 21:30:36 -0700

    adds playlist api and hook. adds playlists to sidebar. validates auth token via middleware. fetches playlists from prisma

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@ import NextImage from 'next/image';
 import NextLink from 'next/link';
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+import { usePlaylist } from '../lib/hooks';
 
 const navMenu = [
   {
@@ -34,9 +35,10 @@ const musicMenu = [
   }
 ]
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+// const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist();
   return (
     <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
       <Box paddingY="20px" height="100%">
@@ -79,11 +81,11 @@ const Sidebar = () => {
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map(playlist => (
-              <ListItem paddingX="20px" fontSize="16px" key={playlist}>
+              <ListItem paddingX="20px" fontSize="16px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/">
                     <LinkOverlay>
-                      {playlist}
+                      {playlist.name}
                     </LinkOverlay>
                   </NextLink>
                 </LinkBox>
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,6 @@ const signedinPages = ['/', '/playlist', '/library'];
 export default function middleware (req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
       const token = req.cookies.TRAX_ACCESS_TOKEN;
-
     if (!token) {
       const url = req.nextUrl.clone()
       url.pathname = '/signin';
